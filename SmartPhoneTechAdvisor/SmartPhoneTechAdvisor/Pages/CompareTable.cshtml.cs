b6eff2fb21c7e204812134c59e5259a5f19af25c
sztrelcsikzoltan <szzoltan@gmx.net>
2023-09-24 15:39:03 +0200

    Bind the API into the application - create the IAiAdvisor interface with the GetPhoneSpecsAsync method to get the phone specifications data from the API - add NuGet package Refit.HttpClientFactory 7.0.0 to provide HTTP client connection to the API - in Program.cs add this service using the extension method AddRefitClient, and configure it with the API’s Uri - in CompareTable.cshtml.cs inject the IaiAdvisor interface into the CompareTableModel, and add the OnGetAsync method as a Task to retrieve the phone specifications data from the API, using the GetPhoneSpecsAsync method of the interface

--- SmartPhoneTechAdvisor/SmartPhoneTechAdvisor/Pages/CompareTable.cshtml.cs
+++ SmartPhoneTechAdvisor/SmartPhoneTechAdvisor/Pages/CompareTable.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SmartPhoneTechAdvisor.HttpClients;
 using SmartPhoneTechAdvisor.Models;
 
 namespace SmartPhoneTechAdvisor.Pages
@@ -10,8 +11,22 @@ namespace SmartPhoneTechAdvisor.Pages
         public List<string> Phones { get; set; } = new();
         public List<AiPhoneSpec> PhoneSpecs { get; set; } = new();
 
-        public void OnGet()
+        private readonly IAiAdvisor _aiAdvisor;
+
+        public CompareTableModel(IAiAdvisor aiAdvisor)
         {
+            _aiAdvisor = aiAdvisor;
         }
+
+        public async Task OnGetAsync()
+        // public async void OnGetAsync()
+        {
+            foreach (var phone in Phones)
+            {
+                var phoneSpec = await _aiAdvisor.GetPhoneSpecsAsync(phone);
+                PhoneSpecs.Add(phoneSpec);
+            }
+        }
+
     }
 }
